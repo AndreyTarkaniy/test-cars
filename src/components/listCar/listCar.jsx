f1b29f9cad102f841b90214d3e526819ba665652
AndreyTarkaniy <endrubass@gmail.com>
2023-09-15 22:43:49 +0300

    create searchBar

---
+++ src/components/listCar/listCar.jsx
@@ -0,0 +1,23 @@
+import { ListWrap } from './listCar.styled';
+
+const ListCar = () => {
+  return (
+    <div>
+      <ListWrap>
+        <ul>
+          <li>Buick</li>
+          <li>Volvo</li>
+          <li>Hummer</li>
+          <li>Subaru</li>
+          <li>Mitsubishi</li>
+          <li>Nissan</li>
+          <li>Lincoln</li>
+          <li>GMC</li>
+          <li>Hyundai</li>
+        </ul>
+      </ListWrap>
+    </div>
+  );
+};
+
+export default ListCar;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,10 @@
+import { SearchBar } from 'components/serchBar/serchBar';
+
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
+      <SearchBar />
     </div>
   );
 };
